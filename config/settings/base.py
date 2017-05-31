be6fbafcecc1afdae7c931a40a38d3fc0e256823
Safouh Dahlan <safouh94@gmail.com>
2017-05-31 15:55:31 +0300

    Part 2: First Commit

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,10 +39,12 @@ DJANGO_APPS = (
 
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 
 LOCAL_APPS = (
+    'project.api',
 )
 
 
@@ -144,3 +146,8 @@ STATICFILES_FINDERS = (
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+
+REST_FRAMEWORK = {
+
+}
